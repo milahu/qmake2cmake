b4f9cc2dac61cef37fddba1a76234f5667efa498
Joerg Bornemann <joerg.bornemann@qt.io>
2022-04-01 16:35:09 +0200

    Add a warning comment to the convenience wrappers
    The wrapper scripts are not used in the qmake2cmake wheel.  Their only
    purpose is to provide a convenience wrapper for people who clone the
    repository.  Mention that no further logic should be added beyond simple
    wrapping.
    
    Change-Id: Iba0a147b8fe9e5289632b2a23f1f03915d9cd603
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

--- run_qmake2cmake.bat
+++ run_qmake2cmake.bat
@@ -1,2 +1,5 @@
 @echo off
+rem This script is not packaged in the qmake2cmake wheel.
+rem It's only purpose is to provide a convenience wrapper for people who clone the repository.
+rem Do not add any logic that goes beyond calling the python module with arguments.
 python %~dp0\run_pro2cmake.py %*
